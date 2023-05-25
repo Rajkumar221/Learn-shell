de5676a275cb56f08b23095df283e0bef4e9c1d4
Rajkumar221 <rajkumarpatel52247@gmail.com>
2023-05-25 12:08:16 +0530

    print

---
+++ 02.print.sh
@@ -0,0 +1,17 @@
+#To print there are couple of commands, but echo is widely used
+
+echo Hello world
+
+#while printing some times to grab the attention of user we might need to print in some color
+#sysntax echo -e "\e[COLmMessage\e[0m"
+# eco -e -enables the color
+#\e[COLm -to enable certain color
+#\e[0m -to disable the color that enabled
+#COL stands for color and possible colors are Red(31), Green(32), Yellow(33), Blue(34), Magenta(35), Cyan(36)
+
+echo -e "\e[31mHello in Red Color\e[0m"
+echo -e "\e[32mHello in Green Color\e[0m"
+echo -e "\e[33mHello in Yellow Color\e[0m"
+echo -e "\e[34mHello in Blue Color\e[0m"
+echo -e "\e[35mHello in Magenta Color\e[0m"
+echo -e "\e[36mHello in Cyan Color\e[0m"
