2efe9e9fd001e064d18f1012f8826938c357cd99
Rajkumar221 <rajkumarpatel52247@gmail.com>
2023-05-26 10:07:52 +0530

    roboshop setup

---
+++ 10.Redirectories.sh
@@ -0,0 +1,8 @@
+#to redirect the output we use > and 1>
+>/tmp/roboshop.log
+1>/tmp/roboshop.log
+# to redirect the error we use 2>
+2>/tmp/roboshop.log
+
+# to nullify the output we use >/dev/null
+>/dev/null
