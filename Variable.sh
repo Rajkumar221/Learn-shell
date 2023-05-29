3bbbc614085c3c15a6b9be585e353c0e9db08170
Rajkumar221 <rajkumarpatel52247@gmail.com>
2023-05-29 10:01:48 +0530

    variable

---
+++ Variable.sh
@@ -0,0 +1,21 @@
+# If we assign a name to a set of data is called as variable.
+#Examples:
+
+a=10
+
+name=john
+
+password=abcd123
+
+#In shell we do not have data types, simply we can declare variable. **(10, John)**
+#variable names  **(a, name**) should be using alphabets and numbers and **_ (underscore).**
+
+
+#When we have some repetitive content than we can declare that in variable and we can use that variable reference everywhere .
+#Advantage is if we change in one place that impacts all the places where that value is used.
+
+a=10
+echo value of a is $A
+
+#alternaively variables also accesed by using ${a}
+echo value of a is ${a}dollers
