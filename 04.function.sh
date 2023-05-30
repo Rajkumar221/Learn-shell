d04b4b29f5e3ccd0824fcb74bb371027a8dcebbb
Rajkumar221 <rajkumarpatel52247@gmail.com>
2023-05-30 07:04:11 +0530

    arguments

--- 04.function.sh
+++ 04.function.sh
@@ -3,3 +3,12 @@ print_hello () {
 }
 
 print_hello
+
+print_arguments() {
+    echo first argument $1
+    echo second argument $2
+    echo all arguments $*
+    echo No of arguments $#
+}
+
+print-arguments ABC 123 XYZ
